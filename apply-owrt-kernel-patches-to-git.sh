231ce5e7437cfca46b4c57c415c8c3a1e51dbbad
Andrew McDonnell <bugs@andrewmcdonnell.net>
2015-01-04 13:35:10 +1030

    Update with wiki instructions at https://github.com/pastcompute/openwrt-cc-ar71xx-hardened/wiki

--- apply-owrt-kernel-patches-to-git.sh
+++ apply-owrt-kernel-patches-to-git.sh
@@ -17,8 +17,8 @@ test -z $VERSION && { echo "Missing VERSION argument (e.g: v3.14.27)" ; exit 1; 
 set -e
 set -x
 
-git branch -f owrt_only_$VERSION
-git checkout owrt_only_$VERSION
+git branch -f owrt-applied-on-$VERSION
+git checkout owrt-applied-on-$VERSION
 
 
 X=${SOURCES}/target/linux/generic/files
@@ -30,14 +30,14 @@ cp -av $X/* .
 X=${SOURCES}/target/linux/generic/patches-3.14
 for x in `cd $X ; ls |sort` ; do
   echo "PATCH: [$X/$x]"
-  patch -f -p1 -E -d. < $X/$x || { bash; true; }
+  patch -f -p1 -E -d. < $X/$x || { echo "Fix any conflicts then exit this shell..." ; bash; true; }
   git add -A ; git commit -m "$x"
 done
 
 X=${OWRT_SOURCES}/target/linux/ar71xx/patches-3.14
 for x in `cd $X ; ls |sort` ; do
   echo "PATCH: [$X/$x]"
-  patch -l -f -p1 -E -d. < $X/$x || { bash; true; }
+  patch -l -f -p1 -E -d. < $X/$x || { echo "Fix any conflicts then exit this shell..." ; bash; true; }
   git add -A ; git commit -m "$x"
 done
 
